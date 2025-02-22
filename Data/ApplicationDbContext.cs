6053f00e629743b0a6e39cdc2bf411368a9c2cb0
alonso-dev <alonso10es@gmail.com>
2025-02-22 13:11:58 -0600

    1-db context finish, databaase connection stablish

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -19,27 +19,40 @@ public class ApplicationDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        // Pet -> PetImages (One-to-Many)
-       modelBuilder.Entity<Pet>()
-        .HasMany( p => p.Images)
-        .WithOne( pi => pi.Pet)
-        .HasForeignKey( pi => pi.PetID)
+    // Pet -> PetImages (One-to-Many)
+    modelBuilder.Entity<Pet>()
+        .HasMany(p => p.Images)
+        .WithOne(pi => pi.Pet)
+        .HasForeignKey(pi => pi.PetID)
+        .OnDelete(DeleteBehavior.Cascade); 
+
+    // AdoptionRequest -> Pet (One-to-Many)
+    modelBuilder.Entity<AdoptionRequest>()
+        .HasOne(ar => ar.Pet)
+        .WithMany()
+        .HasForeignKey(ar => ar.PetID)
+        .OnDelete(DeleteBehavior.Cascade); 
+
+    // AdoptionRequest -> User (One-to-Many)
+    modelBuilder.Entity<AdoptionRequest>()
+        .HasOne(ar => ar.User)
+        .WithMany(u => u.AdoptionRequests)
+        .HasForeignKey(ar => ar.UserID)
+        .OnDelete(DeleteBehavior.Restrict); 
+
+    // AdoptionRequest -> MessageThread (One-to-One)
+    modelBuilder.Entity<AdoptionRequest>()
+        .HasOne(ar => ar.MessageThread)
+        .WithOne(mt => mt.AdoptionRequest)
+        .HasForeignKey<MessageThread>(mt => mt.AdoptionRequestID)
         .OnDelete(DeleteBehavior.Cascade);
 
-        // AdoptionRequest -> MessageThread (One-to-One)
-        modelBuilder.Entity<AdoptionRequest>()
-            .HasOne( ar => ar.MessageThread)
-            .WithOne( mt => mt.AdoptionRequest)
-            .HasForeignKey<MessageThread>( mt => mt.AdoptionRequestID)
-            .OnDelete(DeleteBehavior.Cascade);
-
-        // MessageThread -> Messages (One-to-Many)
-        modelBuilder.Entity<MessageThread>()
-            .HasMany( mt => mt.Messages )
-            .WithOne( m => m.MessageThread)
-            .HasForeignKey( m => m.MessageThreadID )
-            .OnDelete(DeleteBehavior.Cascade);
-
+    // MessageThread -> Messages (One-to-Many)
+    modelBuilder.Entity<MessageThread>()
+        .HasMany(mt => mt.Messages)
+        .WithOne(m => m.MessageThread)
+        .HasForeignKey(m => m.MessageThreadID)
+        .OnDelete(DeleteBehavior.Cascade);
             
     }
 
--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using PetAdoptionAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>( options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
